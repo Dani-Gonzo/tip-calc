09ff12dbc6823221c6eff6fca5560ac46ad67ff8
Book Dragon <d.gonso@hotmail.com>
2020-02-07 10:21:24 -0800

    fixed up display and functionality. refactored to use state

--- src/components/CalcUI.js
+++ src/components/CalcUI.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const CalcUI = () => {
-    let total;
+export default class CalcUI extends Component {
+    state = {
+        billAmt: "",
+        serviceQual: 0,
+        numPeople: "",
+        total: ""
+    }
 
-    function calculateTip() {
-        const billAmt = document.getElementById("billamt").value;
-        const serviceQual = document.getElementById("serviceQual").value;
-        let numPeople = document.getElementById("peopleamt").value;
+    calculateTip = () => {
+        const billAmt = this.state.billAmt;
+        const serviceQual = this.state.serviceQual;
+        let numPeople = this.state.numPeople;
 
         if (billAmt === "" || serviceQual == 0) {
             alert("Please enter values");
@@ -15,54 +20,61 @@ const CalcUI = () => {
 
         if (numPeople === "" || numPeople <= 1) {
             numPeople = 1;
-            document.getElementById("each").style.display = "none";
-        } else {
-            document.getElementById("each").style.display = "block";
-        }
+        } 
 
-        total = (billAmt * serviceQual) / numPeople;
+        let total = (billAmt * serviceQual) / numPeople;
         total = Math.round(total * 100) / 100;
         total = total.toFixed(2);
-        document.getElementById("totalTip").style.display = "block";
-        document.getElementsById("tip").innerHTML = total;
+        this.setState({total: total});
     }
 
-    let tipContainer;
-
-    if (total) {
-        tipContainer = <div id="totalTip">
-                            <sup>$</sup>
-                            <span id="tip">0.00</span>
-                            <small id="each"> each</small>
-                        </div>
+    changeHandler = e => {
+        let id = e.target.id;
+        let value = e.target.value;
+        this.setState({[id]: value})
     }
 
-    return (
-        <div id="container">
-            <h1>Tip Calculator</h1>
-            <div id="calculator">
-                <form>
-                    <p>How much was your bill?</p>
-                    <p>$ <input id="billamt" type="text" placeholer="Bill Amount"></input></p>
-                    <p>How was your service?</p>
-                    <p>
-                        <select id="serviceQual">
-                            <option disabled selected value="0">-- Choose an Option --</option>
-                            <option value="0.25">25% Outstanding</option>
-                            <option value="0.2">20% Good</option>
-                            <option value="0.15">15% Okay</option>
-                            <option value="0.10">10% Bad</option>
-                            <option value="0.05">5% Terrible</option>
-                        </select>
-                    </p>
-                </form>
-                <p>How many people are sharing the bill?</p>
-                <input id="peopleamt" type="text" placeholder="Number of People" />
-                <button type="button" id="calculate" onClick={calculateTip}>Calculate Tip</button>
+    render() {
+        let each;
+        let tipContainer;
+
+        if (this.state.numPeople > 1) {
+            each = <small id="each"> each</small>
+        }
+
+        if (this.state.total) {
+            tipContainer = <div id="totalTip">
+                                <sup>$</sup>
+                                <span id="tip">{this.state.total}</span>
+                                {each}
+                            </div>
+        }
+
+        return (
+            <div id="container">
+                <h1>Tip Calculator</h1>
+                <div id="calculator">
+                    <form>
+                        <p>How much was your bill?</p>
+                        <p>$ <input id="billAmt" type="text" onChange={this.changeHandler} placeholer="Bill Amount"></input></p>
+                        <p>How was your service?</p>
+                        <p>
+                            <select id="serviceQual" value={this.state.serviceQual} onChange={this.changeHandler}>
+                                <option disabled value="0">-- Choose an Option --</option>
+                                <option value="0.25">25% Outstanding</option>
+                                <option value="0.2">20% Good</option>
+                                <option value="0.15">15% Okay</option>
+                                <option value="0.10">10% Bad</option>
+                                <option value="0.05">5% Terrible</option>
+                            </select>
+                        </p>
+                    </form>
+                    <p>How many people are sharing the bill?</p>
+                    <p><input id="numPeople" type="text" onChange={this.changeHandler} placeholder="Number of People" /></p>
+                    <button type="button" id="calculate" onClick={this.calculateTip}>Calculate Tip</button>
+                </div>
+                {tipContainer}
             </div>
-            {tipContainer}
-        </div>
-    );
+        )
+    }
 }
-
-export default CalcUI;
