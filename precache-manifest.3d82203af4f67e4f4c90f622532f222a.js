7c445eff35c867d32036f53fbb8b9f8b194abe27
Book Dragon <d.gonso@hotmail.com>
2020-02-07 16:13:41 -0800

    Updates

---
+++ precache-manifest.3d82203af4f67e4f4c90f622532f222a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3e4d599bac4adb63898901a5e110f4",
+    "url": "/tip-calc/index.html"
+  },
+  {
+    "revision": "5c3d5b83a506e1ee6da0",
+    "url": "/tip-calc/static/css/main.782faa3c.chunk.css"
+  },
+  {
+    "revision": "60ea5a4de0d69c19faa6",
+    "url": "/tip-calc/static/js/2.a746512d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tip-calc/static/js/2.a746512d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3d5b83a506e1ee6da0",
+    "url": "/tip-calc/static/js/main.c0cac134.chunk.js"
+  },
+  {
+    "revision": "7ab6862dd4f80b8b1129",
+    "url": "/tip-calc/static/js/runtime-main.3993293a.js"
+  }
+]);
